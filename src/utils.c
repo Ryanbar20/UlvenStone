7a67e47b8050bd4a027aa5d0d1d8d03a03938e24
Ryan <r.bartelds@student.utwente.nl>
2025-09-19 19:58:07 +0200

    bugfixes

--- src/utils.c
+++ src/utils.c
@@ -29,12 +29,7 @@ static inline struct v2_f rotate(float rad, struct v2_f vector) {
 }
 
 // returns -1 if not hit, else returns distance to hit
-int check_hit(struct v2_f ray,struct v2_f position, struct v2_f w1, struct v2_f w2) {
-    //hit if:
-    //  1: wall.v1 is on the other side of the ray that wall.v2 is on
-    //  2: the place that is hit is closer to pos than the length of the ray
-
-    // 1:
+float check_hit(struct v2_f ray,struct v2_f position, struct v2_f w1, struct v2_f w2) {
     int v1_under;
     int v2_under;
     float a_ray = -1;
@@ -42,37 +37,34 @@ int check_hit(struct v2_f ray,struct v2_f position, struct v2_f w1, struct v2_f 
     if (ray.x == 0) {
         v1_under = w1.x > position.x;
         v2_under = w2.x > position.x;
-
     } else {
 
-        float a_ray = ray.y / ray.x ;
-        float b_ray = position.y - a_ray * position.x;
+        a_ray = ray.y / ray.x ;
+        b_ray = position.y - a_ray * position.x;
         v1_under = w1.y < (w1.x * a_ray + b_ray);
         v2_under = w2.y < (w2.x* a_ray + b_ray);
     }
-    if (v1_under == v2_under) {
-        // both are on the same side of the ray -> no hit
-        return -1;
+    if (v1_under == v2_under) { // if wall is parralel, it wont hit
+        return (float) -1;
     }
     
 
-    // check if wall or ray is exactly vertical
     float hit_x;
     float hit_y;
     if (w1.x-w2.x ==0) {
-        //.... 
-        // close enough if either point of the wall is in render distance
         float dist = get_distance(w1,position);
-        if (dist <= RENDER_DIST) {
+        // dist2 is to check that the wall is in front of the player.
+        float dist2 = get_distance(w1,set_length(RENDER_DIST,ray));
+        if (dist <= RENDER_DIST && dist2 < RENDER_DIST) {
             return dist;
         }
         dist = get_distance(w2,position);
-        if (dist <= RENDER_DIST) {
+        dist2 = get_distance(w2,set_length(RENDER_DIST,ray));
+        if (dist <= RENDER_DIST && dist2 < RENDER_DIST) {
             return dist;
         }
-        return -1;
+        return (float) -1;
     } else if (ray.x ==0) {
-        // get hit point by plugging pos.x in into the wall-line and then check dist
         float a_wall = (w1.y-w2.y) / (w1.x-w2.x);
         float b_wall = w1.y - a_wall * w1.x;
         hit_x = position.x;
@@ -80,12 +72,13 @@ int check_hit(struct v2_f ray,struct v2_f position, struct v2_f w1, struct v2_f 
     } else {
         float a_wall = (w1.y-w2.y) / (w1.x-w2.x);
         float b_wall = w1.y - a_wall * w1.x;
-        hit_x = (b_wall - b_ray) / (a_ray - a_wall);
+        hit_x = (b_ray - b_wall) / (a_wall - a_ray);
         hit_y = a_wall * hit_x + b_wall;
     }
     float dist = get_distance((struct v2_f) {hit_x,hit_y}, position);
-    if (dist <= RENDER_DIST) {
+    float dist2 = get_distance((struct v2_f) {hit_x,hit_y},set_length(RENDER_DIST,ray));
+    if (dist <= RENDER_DIST && dist2 < RENDER_DIST) {
         return dist;
     }
-    return -1;
+    return (float) -1;
 }
