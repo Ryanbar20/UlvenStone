a97aa77e9ec20f51feaef5cd69989ace7b151bc1
Ryan <r.bartelds@student.utwente.nl>
2025-09-21 14:44:11 +0200

    cleanup

--- src/main.c
+++ src/main.c
@@ -7,11 +7,11 @@
 
 #define HEIGHT  560
 #define WIDTH   1000
-#define FOV 100
 #define RENDER_DIST 100
 #define WALL_HEIGHT 0.1f
-#define SCREEN_DIST 1
-#define CAM_WIDTH 1.5f
+
+#define SCREEN_DIST 1   // corresponds to FOV (higher means less FOV)
+#define CAM_WIDTH 1.5f  // corresponds to FOV (higher means more FOV) (currently around 85deg)
 #include "utils.c"
 #include "world_loader.c"
 
@@ -49,48 +49,51 @@ SDL_Renderer* createRenderer(SDL_Window* window) {
 }
 
 
+v2_f get_column(float distance) {
+    int y_lo = (int) (HEIGHT/2.0f - (WALL_HEIGHT/2.0f)/distance * HEIGHT / (WALL_HEIGHT/2.0f));
+    int y_hi = (int) (HEIGHT/2.0f + (WALL_HEIGHT - (WALL_HEIGHT/2.0f))/distance * HEIGHT / (WALL_HEIGHT/2.0f));
+    y_lo = y_lo < 0 ? 0 : y_lo;
+    y_hi = y_hi >= HEIGHT ? HEIGHT-1 : y_hi;
+    return (v2_f) {y_lo, y_hi};
+}
+
+static inline v2_f get_ray(int screen_index,v2_f perpendicular) {
+    float dw = (CAM_WIDTH / 2.0f - ((CAM_WIDTH * screen_index)/WIDTH));
+    return set_length(1,(v2_f) {dw*perpendicular.x + view.x,dw*perpendicular.y + view.y});
+}
 
 
 void cast_rays(SDL_Renderer* renderer,struct World* world) {
-    struct Wall* wall_hit;
-    float wall_hit_distance;
+    //declare loop variables
+    struct Wall* wall_hit;  float wall_hit_distance;    v2_f ray;
+    //constant static variables
+    const v2_f perpendicular = rotate(DEG_TO_RAD(90),view);
     for (int i =0; i<WIDTH; i++) {
-        v2_f perpendicular = rotate(DEG_TO_RAD(90),view);
-        float dw = (CAM_WIDTH / 2.0f - ((CAM_WIDTH * i)/WIDTH));
-        v2_f ray = set_length(1,(v2_f) {dw*perpendicular.x + view.x,dw*perpendicular.y + view.y});
-        float angle = get_angle_between_vectors(ray,view);
-        wall_hit = NULL;
-        wall_hit_distance = RENDER_DIST + 10;
-
+        //assign loop variables
+        wall_hit = NULL;    wall_hit_distance = RENDER_DIST + 10;   ray = get_ray(i,perpendicular);
+        //constant variables
+        const float angle = get_angle_between_vectors(ray,view);
+        
+        //determine wall to draw, if any
         for (int j=0; j<world->wall_amount; j++) {
-            struct Wall w = world->walls[j];
-            float d = check_hit(ray,pos,w.v1, w.v2);
+            //variables for this iteration
+            const struct Wall w = world->walls[j];
+            const float d = check_hit(ray,pos,w.v1, w.v2);
             if (d >= 0 && d < wall_hit_distance) {
-                wall_hit = world->walls + j;
-                wall_hit_distance = d;
+                wall_hit = world->walls + j;    wall_hit_distance = d;
             }
         }
-        if (wall_hit == NULL) {
-            continue;
-        }
-        float corrected_dist = wall_hit_distance*cos(angle);
-        int y_lo = (int) (HEIGHT/2.0f - (WALL_HEIGHT/2.0f)/corrected_dist * HEIGHT / (WALL_HEIGHT/2.0f));
-        int y_hi = (int) (HEIGHT/2.0f + (WALL_HEIGHT - (WALL_HEIGHT/2.0f))/corrected_dist * HEIGHT / (WALL_HEIGHT/2.0f));
-        y_lo = y_lo < 0 ? 0 : y_lo;
-        y_hi = y_hi >= HEIGHT ? HEIGHT-1 : y_hi;
-        SDL_Rect wall_piece = {
-            i,
-            y_lo,
-            1,
-            y_hi-y_lo
-        };
+
+        //skip drawing stage if no wall has been hit
+        if (wall_hit == NULL) continue;
+        //create pixel boundaries for column
+        const v2_f column = get_column(wall_hit_distance*cos(angle));
+
+        //draw column with corresponding wall color
         SDL_SetRenderDrawColor(renderer,wall_hit->r,wall_hit->g,wall_hit->b,255);
-        SDL_RenderFillRect(renderer,&wall_piece);
+        SDL_RenderDrawLine(renderer,i,column.x,i,column.y);
 
-    }
-
-
-    
+    }   
 }
 
 
